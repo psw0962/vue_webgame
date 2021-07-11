2fd9616014c57479ff69bdf504f0403971ad8722
psw0962 <psw0962@naver.com>
2021-07-11 22:06:08 +0900

    가위바위보 스프린트 시작

---
+++ 5.RockScissorPaper/RockScissorPaper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div id="computer" v-bind:style="computedStyleObject"></div>
+
+    <div>
+      <button @click="onClickButton('바위')">바위</button>
+      <button @click="onClickButton('가위')">가위</button>
+      <button @click="onClickButton('보')">보</button>
+    </div>
+
+    <div>{{ result }}</div>
+    <div>현재 {{ score }}점</div>
+  </div>
+</template>
+
+<script>
+const rspCoords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px",
+};
+
+export default {
+  data() {
+    return {
+      imgCoord: rspCoords.바위,
+      result: "",
+      score: 0,
+    };
+  },
+  computed: {
+    computedStyleObject() {
+      return { background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0` };
+    },
+  },
+  methods: {
+    onClickButton(choice) {},
+  },
+};
+</script>
+
+<style scoped>
+#computer {
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
+}
+</style>
