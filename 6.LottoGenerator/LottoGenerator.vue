2a51c2001d795162fad4f497131adcd06e051713
psw0962 <psw0962@naver.com>
2021-07-14 23:11:44 +0900

    LottoGenerator sprint 시작

---
+++ 6.LottoGenerator/LottoGenerator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>당첨 숫자</div>
+
+    <div id="result">
+      <lotto-ball v-for="ball in winBalls" :key="ball"></lotto-ball>
+    </div>
+
+    <div>보너스</div>
+    <lotto-ball></lotto-ball>
+    <button v-if="redo">한 번 더!</button>
+  </div>
+</template>
+
+<script>
+import LottoBall from "./LottoBall.vue";
+export default {
+  components: { LottoBall },
+  data() {
+    return {
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    };
+  },
+  computed: {},
+  methods: {},
+  mounted: {},
+};
+</script>
+
+<style scoped></style>
